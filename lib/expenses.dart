fbe9bdc49a419d7d347f65fdea2810307b956fbd
Adabe Desmond <adabedesmond@gmail.com>
2023-07-28 20:51:13 +0000

    committing file

---
+++ lib/expenses.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatelessWidget {
+  const Expenses({super.key, required this.title});
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title,
+        style: const  TextStyle(
+          fontWeight: FontWeight.w500
+        ),
+        ),
+        centerTitle: true,
+        leading: const Icon(Icons.arrow_back),
+      ),
+      body: const Center(
+        child: Text(
+          "Get ready",
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 30,
+          ),
+        ),
+      ),
+    );
+  }
+}

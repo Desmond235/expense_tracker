9c212c915063c699bafe3969af30102496970903
Adabe Desmond <adabedesmond@gmail.com>
2023-07-28 21:31:29 +0000

    modified file

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
-class Expenses extends StatelessWidget {
+class Expenses extends StatefulWidget {
   const Expenses({super.key, required this.title});
   final String title;
 
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title,
+        title: Text(widget.title,
         style: const  TextStyle(
           fontWeight: FontWeight.w500
         ),
@@ -17,14 +22,25 @@ class Expenses extends StatelessWidget {
         centerTitle: true,
         leading: const Icon(Icons.arrow_back),
       ),
-      body: const Center(
-        child: Text(
-          "Get ready",
-          style: TextStyle(
-            fontWeight: FontWeight.w500,
-            fontSize: 30,
-          ),
-        ),
+      body: Column(
+        children: [
+          const Center(
+             child: Text(
+              "Get ready",
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 30,
+              ),
+                     ),
+           ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.2,
+            height: 200,
+            child: const Placeholder( 
+              child: Center(child: Text('Expenses')),
+            ),
+          )
+        ],
       ),
     );
   }

b12a8ef393dba224ab67db599a7922c871aabed1
Adabe Desmond <adabedesmond@gmail.com>
2023-08-04 00:22:09 +0000

    modified file

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,20 +1,61 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
- class NewExpense extends StatefulWidget {
-   const NewExpense({super.key});
- 
-   @override
-   State<NewExpense> createState() => _NewExpenseState();
- }
- 
- class _NewExpenseState extends State<NewExpense> {
-   @override
-   Widget build(BuildContext context) {
-     return const Padding(padding: EdgeInsets.all(16),
-     child: TextField(
-      maxLength: 60,
-      decoration: InputDecoration(label: Text('Title')),
-     ),
-     );
-   }
- }
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() => _NewExpenseState();
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    amountController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            maxLength: 60,
+            controller: titleController,
+            decoration: const InputDecoration(
+              label: Text('Title'),
+            ),
+          ),
+          TextField(
+            controller: amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              label: Text('Amount'),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('save expenses'),
+              ),
+              const Spacer(),
+              TextButton(
+                onPressed: () {},
+                child: const Text('Cancel'),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
